ce5b80a5275ef7284e0679a2d149242a3a400136
Роман-ПК\Роман <rosh@drevlanka.ru>
2018-10-21 09:20:59 +0300

    first program

---
+++ FirstProgram/FirstProgram/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
